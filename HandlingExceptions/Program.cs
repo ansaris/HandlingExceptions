d575029f256a4cf37f03afa513cfdf2fd9951907
ansaris <salman.ansari.usa@gmail.com>
2016-04-29 17:59:28 -0500

    Initial commit.

--- HandlingExceptions/Program.cs
+++ HandlingExceptions/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.IO;
 
 namespace HandlingExceptions
 {
@@ -10,6 +12,10 @@ namespace HandlingExceptions
     {
         static void Main(string[] args)
         {
+
+            string content = File.ReadAllText(@"C:\Lesson\Example.txt");
+            Console.WriteLine(content);
+            Console.ReadLine();
         }
     }
 }
