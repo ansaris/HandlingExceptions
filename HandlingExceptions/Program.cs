c75f22f2dc52100cdb350c44c3a0446104953b51
ansaris <salman.ansari.usa@gmail.com>
2016-05-02 14:43:05 -0500

    Use try, catch, and finally. Finish lesson.

--- HandlingExceptions/Program.cs
+++ HandlingExceptions/Program.cs
@@ -12,9 +12,31 @@ namespace HandlingExceptions
     {
         static void Main(string[] args)
         {
-
-            string content = File.ReadAllText(@"C:\Lesson\Example.txt");
-            Console.WriteLine(content);
+            try
+            {
+                string content = File.ReadAllText(@"C:\Lesson\Example.txt");
+                Console.WriteLine(content);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine("The file may not exist or is not named 'Example.txt'.");
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine(@"The directory C:\Lesson\ may not exist or is not named correctly.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was a problem!");
+                Console.WriteLine(ex.Message);
+            }
+            finally 
+            {
+                // Cleaning up, setting values to null, closing database connections.
+                Console.WriteLine("The application will close.");
+            }
             Console.ReadLine();
         }
     }
